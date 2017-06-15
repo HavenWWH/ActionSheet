f2a2c49d5ea9b311633512e93df13df27b40ecf6
Haven <513433750@qq.com>
2017-06-15 13:40:53 +0800

    Initial Commit

---
+++ ActionSheet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ActionSheet
+//
+//  Created by Haven on 2017/6/15.
+//  Copyright © 2017年 com.RuanZhiHongYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
